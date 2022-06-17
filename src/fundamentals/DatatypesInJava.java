4e90f7200509ff571ba11040e76db22b44e1e1c9
nain333 <hnain70@gmai.com>
2022-06-17 15:44:01 +0530

    Firt release of Fundamentals of java.

---
+++ src/fundamentals/DatatypesInJava.java
@@ -0,0 +1,21 @@
+package fundamentals;
+
+public class DatatypesInJava {
+	public static void main(String[] args) {
+		int a = 10;
+		short s = 15;
+		byte b = 11;
+		long l = 117;
+//		DATA TYPES TO STORE DECIMAL NUMBERS:
+		double d = 10.543578999998877777788899999999; 
+		float f  = 10.523344444555555554444444444444f;
+		System.out.println("The float is: "+f+" and the double is "+d);
+//		precision difference is something which is there among double and float. Double is more accurate as it can store more numbers to momre decimal places.
+//		DATA TYPES FOR STORING CHARACTERS.
+		char ch = 'a';
+		System.out.println(ch + " is a character");
+		
+		
+	}
+
+}
